4ffdbb7f16d9997dc4df31f557ae5a1db2d60b46
Oleksandr <OlexandrTanasienko@gmail.com>
2021-02-22 18:33:22 +0200

    setlocale to Russian

--- TextFilesProcessing/TextFilesProcessing.cpp
+++ TextFilesProcessing/TextFilesProcessing.cpp
@@ -1,13 +1,16 @@
 ﻿#include <iostream>
 #include <vector>
+#include <locale>
 #include "student.h"
 #include "ratingTable.h"
 #include "processStudentsData.h"
 
 using namespace std;
 
+
 int main()
 {
+	setlocale(LC_ALL, "Russian");
 	string directory=getNameOfDirectory();
     vector <student> allStudents=processDirectoryWithStudentsData(directory);
 	
