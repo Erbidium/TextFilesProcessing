26fd7d8d6c1d7e56cdccea74b6eddbc766b526ff
Oleksandr <OlexandrTanasienko@gmail.com>
2021-02-22 17:23:06 +0200

    Write for loop to process all files, get students last name

--- TextFilesProcessing/processStudentsData.cpp
+++ TextFilesProcessing/processStudentsData.cpp
@@ -17,9 +17,16 @@ string getNameOfDirectory()
 
 vector<student> processDirectoryWithStudentsData(string directory)
 {
-	vector <string> files;
+	vector <string> files, allStudents;
+	unsigned int sumOfGrades=0;
+	student studentData;
 	for (const auto & entry : filesystem::directory_iterator(directory))
         files.push_back(entry.path().string());
-	
+	for(int i=0;i<files.size();i++)
+	{
+		ifstream fIn(files[i]);
+		fIn>>studentData.lastName;
+		
+	}
 }
 
