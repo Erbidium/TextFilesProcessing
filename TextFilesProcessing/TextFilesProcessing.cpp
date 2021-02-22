f28277bea66648ad5272a0a1ccebfe65bf21fbec
Oleksandr <OlexandrTanasienko@gmail.com>
2021-02-22 16:46:36 +0200

    Use function getNameOfDirectory in main() and include headers

--- TextFilesProcessing/TextFilesProcessing.cpp
+++ TextFilesProcessing/TextFilesProcessing.cpp
@@ -1,10 +1,14 @@
 ﻿#include <iostream>
 #include <vector>
 #include "student.h"
+#include "ratingTable.h"
+#include "processStudentsData.h"
 
 using namespace std;
 
 int main()
 {
+	string directory=getNameOfDirectory();
     vector <student> allStudents;
+	
 }
