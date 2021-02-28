dc9ecad2709c55f79fb2f8ff023a71250635793e
Danil <danilsmy.edu@gmail.com>
2021-02-28 12:25:07 +0200

    Add a file with results opening check

--- TextFilesProcessing/ratingTable.cpp
+++ TextFilesProcessing/ratingTable.cpp
@@ -21,13 +21,21 @@ void makeOutFile(vector<student> allStudents,string directory){
 	ofstream fOut;
 	string finalDirectory = directory + "/Result.csv";
 	fOut.open(finalDirectory);
-	int k = 0;
-	for (int i = 0; i < allStudents.size(); i++) {
-		if(k <= (allStudents.size() * 0.4)) {
-			if(!allStudents[i].isTuitionPaying){
-				fOut << allStudents[i].lastName << ","<< allStudents[i].averageScore << "," << endl;
-				k++;
+	if (!fOut) {
+		cout << "Cannot open a file \n";
+	}
+	else {
+		int k = 0;
+		double minGrade = 0;
+		for (int i = 0; i < allStudents.size(); i++) {
+			if (k <= (allStudents.size() * 0.4)) {
+				if (!allStudents[i].isTuitionPaying) {
+					fOut << allStudents[i].lastName << "," << allStudents[i].averageScore << "," << endl;
+					k++;
+					minGrade = allStudents[i].averageScore;
+				}
 			}
 		}
+		cout << "\nMinimal grade for scholarship:" << minGrade << endl;
 	}
 }
